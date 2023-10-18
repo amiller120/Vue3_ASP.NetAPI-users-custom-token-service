781b71fd04a4324d96d163b74dfa5032595faa9a
LazyTitan <adam5bucks475@gmail.com>
2023-10-18 19:00:29 -0400

    Initial commit

---
+++ fastEndpointTeamplateTest/vue-client/src/layouts/default/Default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Your Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- Add a navigation drawer -->
+    <v-navigation-drawer v-model="drawer" location="left">
+      <v-list-item title="Shopify Demo" subtitle="A&C LLC"></v-list-item>
+      <v-divider></v-divider>
+
+
+      <v-list-item link title="Home"><router-link to="/"></router-link></v-list-item>
+      <v-list-item link title="List Item 2"><router-link to="/about"></router-link></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from "vue"
+  const drawer = ref(false);
+  const items = ref([{ title: "Home", path: '/' }, { title: "About", path: '/About' }])
+
+
+</script>
---
+++ fastEndpointTeamplateTest/vue-client/src/layouts/default/View.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout>
+    <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+
+    <v-app-bar color="primary" prominent>
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" location="left">
+      <v-list-item title="Shopify Demo" subtitle="A&C LLC"></v-list-item>
+      <v-divider></v-divider>
+      
+      
+      <v-list-item link title="Home"><router-link to="/"></router-link></v-list-item>
+      <v-list-item link title="List Item 2"><router-link to="/about"></router-link></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
+    </v-navigation-drawer>
+
+    <v-main style="height: 500px;">
+      <slot>
+        <router-view />
+      </slot>
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const drawer = ref(false);
+</script>
