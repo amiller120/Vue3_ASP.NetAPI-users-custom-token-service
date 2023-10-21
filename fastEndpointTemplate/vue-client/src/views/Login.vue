8f8944db05d83cf92e649aaf0e946ecb2481bd93
LazyTitan <adam5bucks475@gmail.com>
2023-10-21 13:22:45 -0400

    after registration or login route to a different page.

--- fastEndpointTemplate/vue-client/src/views/Login.vue
+++ fastEndpointTemplate/vue-client/src/views/Login.vue
@@ -5,21 +5,24 @@
       <v-text-field v-model="password" label="Password" type="password"></v-text-field>
       <v-btn @click="login">Login</v-btn>
       <router-link to="/register"><v-btn>Register</v-btn></router-link>
-</v-form>
+    </v-form>
   </v-container>
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue';
-import AuthService from '../Services/Api/AuthService';
+  import AuthService from '../Services/Api/AuthService';
+  import { useRouter } from 'vue-router'
 
   const email = ref('');
   const password = ref('');
+  const router = useRouter();
 
-  const login = async() => {
+  const login = async () => {
     let loginResponse = await AuthService.Login(email.value, password.value);
-    if (loginResponse.status == 200) {      
+    if (loginResponse.status == 200) {
       document.cookie = `access_token=${loginResponse.data.accessToken}; path=/; expires=${loginResponse.data.accessExiry}SameSite=None; Secure=True;`;
+      router.push("/");
     }
   }
 </script>
