7ffc11ecd8634fb4e5f34a7fc10a81ccf9881703
LazyTitan <adam5bucks475@gmail.com>
2023-10-20 20:02:21 -0400

    fix width of login and register forms.

--- fastEndpointTemplate/vue-client/src/views/Login.vue
+++ fastEndpointTemplate/vue-client/src/views/Login.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-form>
-    <v-text-field v-model="email" label="Email"></v-text-field>
-    <v-text-field v-model="password" label="Password" type="password"></v-text-field>
-    <v-btn @click="login">Login</v-btn>
-    <v-btn @click="register">Register</v-btn>
-  </v-form>
+  <v-container class="d-flex justify-center">
+    <v-form class="w-50">
+      <v-text-field v-model="email" label="Email"></v-text-field>
+      <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+      <v-btn @click="login">Login</v-btn>
+      <v-btn @click="register">Register</v-btn>
+    </v-form>
+  </v-container>
 </template>
 
 <script setup lang="ts">
